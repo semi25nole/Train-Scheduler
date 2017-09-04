31c64604c68b639ce601fd1c2cf11b1ad60ba5f1
Kyle Ruschel <dkr1985@icloud.com>
2017-09-04 15:08:15 -0400

    Initial Commit

---
+++ assets/js/train.js
@@ -0,0 +1,9 @@
+var config = {
+   apiKey: "AIzaSyBtmXpIiX7akXlaIdiV8PwifeH7vlqV2zQ",
+   authDomain: "train-departures.firebaseapp.com",
+   databaseURL: "https://train-departures.firebaseio.com",
+   projectId: "train-departures",
+   storageBucket: "train-departures.appspot.com",
+   messagingSenderId: "75786081357"
+ };
+ firebase.initializeApp(config);
