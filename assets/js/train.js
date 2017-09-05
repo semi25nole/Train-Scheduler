fff05a539e73b3c541b967b0d2333689df8dbf32
Kyle Ruschel <dkr1985@icloud.com>
2017-09-04 20:35:42 -0400

    updates

--- assets/js/train.js
+++ assets/js/train.js
@@ -7,3 +7,47 @@ var config = {
    messagingSenderId: "75786081357"
  };
  firebase.initializeApp(config);
+
+ var database = firebase.database();
+
+ var tName = "";
+ var dest = "";
+ var freq = "";
+ var nxtArr = "";
+ var minA = "";
+ var table = $('')
+
+ $('.btn-primary').on("click", function() {
+   event.preventDefault();
+
+   tName = $('#tName').val().trim();
+   dest = $('#dest').val().trim();
+   freq = $('#freq').val().trim();
+
+
+   database.ref().set({
+        tName: tName,
+        dest: dest,
+        freq: freq,
+        nxtArr: nxtArr
+      });
+
+      database.ref().on("value", function(snapshot) {
+
+            // Print the initial data to the console.
+            console.log(snapshot.val());
+
+            // Log the value of the various properties
+            console.log(snapshot.val().tName);
+            console.log(snapshot.val().dest);
+            console.log(snapshot.val().freq);
+            console.log(snapshot.val().nxtArr);
+
+            // Change the HTML
+            $("#new").html(snapshot.val().tName + " | " + snapshot.val().dest + " | " + snapshot.val().freq + " | " + snapshot.val().nxtArr);
+
+            // If any errors are experienced, log them to console.
+          }, function(errorObject) {
+            console.log("The read failed: " + errorObject.code);
+          });
+    });
